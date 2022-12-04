ec25c913ceea64c6f911b3d98cbe5d9fccf0cb14
Kitalfa <dorian.travers@yahoo.fr>
2022-12-04 11:48:24 +0100

    header

---
+++ README.md
@@ -0,0 +1,1 @@
+# Kitalfa.github.io
---
+++ js/javascript.js
@@ -0,0 +1,13 @@
+const buttonContact = document.querySelector(".header__button")
+
+function contactForm () {
+     const form = document.querySelector(".formContact")
+     form.classList.toggle("hidden")
+    
+}
+    
+
+    
+buttonContact.addEventListener("click", contactForm)
+
+console.log(addEventListener)
