6c2b29ca3b304c3cc44a3d3331d92ffe8bbafe0f
Kitalfa <dorian.travers@yahoo.fr>
2023-01-08 20:22:29 +0100

    Slide1 information

--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,5 @@
+// Formulaire
+
 const buttonContact = document.querySelector(".header__button")
 
 function contactForm () {
@@ -11,3 +13,30 @@ function contactForm () {
 buttonContact.addEventListener("click", contactForm)
 
 console.log(addEventListener)
+
+// Progress bar age 
+
+
+
+const daysUntilNextYear = get_days_until_next_year();
+const Percent = moveProgressBar();
+
+function get_days_until_next_year()
+{
+     let Date1 = new Date(); // aujourd'hui
+     let Year = Date1.getFullYear();
+     let Date2 = new Date((Year+1)+'-01-01'); // 1er janvier de l'année suivante
+     let nbre_Jours = Math.ceil((Date2 - Date1)/(1000*60*60*24)); // .ceil : inclus aujourd'hui (sinon .floor)
+     return nbre_Jours;
+}
+     
+function moveProgressBar() {   
+    
+     let dayNow = 365 - daysUntilNextYear;
+     let getPercent = (dayNow * 1) / 365;  
+     return getPercent   
+}
+
+const progressBar = document.querySelector(".information__progressBar");
+progressBar.style.width = Percent + '%';
+
